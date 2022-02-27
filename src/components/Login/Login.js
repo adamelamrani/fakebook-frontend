85cd43ecf25970d543cc5efc3d28e85a6457a6e7
Adam El Amrani <adelamco1091@outlook.com>
2022-02-27 20:47:01 +0100

    Added navigation to usermainpage

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import StyledLogin from "./StyledLogin";
 import { useDispatch } from "react-redux";
 import { loginUserThunk } from "../../redux/thunks/thunks";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const blankLogin = {
@@ -19,15 +20,15 @@ const Login = () => {
   };
 
   const invalidForm = formData.username === "" || formData.password === "";
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const logUser = (event) => {
     event.preventDefault();
     dispatch(loginUserThunk(formData));
+    navigate("/main-page");
   };
   return (
     <>
-      <h2>fakebook</h2>
       <StyledLogin onSubmit={logUser} className="login-form">
         <label htmlFor="username">Username:</label>
         <input
